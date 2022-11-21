df9480b28d04a836b9bc5d57625d0abced044df9
unknown <shushila>
2022-11-21 11:18:21 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 style={{ backgroundColor: "#3BDEF1", fontSize: "50px", padding: "20px", color: "white", fontFamily: "cursive", marginTop: "0" }}>COUNTER APP</h1>
+        <h1 style={{ fontSize: "40px", color: "#505050" }}>COUNT : {count}</h1>
+
+        <button className="button" onClick={() => setCount(count + 1)} >Count + 1</button>
+        <button className="button" onClick={() => {
+          if (count <= 0) {
+            setCount(0)
+          } else {
+            setCount(count - 1)
+          }
+        }} >Count - 1</button>
+        <br></br>
+        <button className="button" onClick={() => setCount(count + 5)} >Count + 5</button>
+        <button className="button" onClick={() => setCount(count + 10)} >Count + 10</button>
+        <button className="button" onClick={() => setCount(count + 100)} >Count + 100</button>
+        <br></br>
+        <button className="button" onClick={() => setCount(count - 5)} >Count - 5</button>
+        <button className="button" onClick={() => setCount(count - 10)} >Count - 10</button>
+        <button className="button" onClick={() => setCount(count - 100)} >Count - 100</button>
+        <br></br>
+        <button className="button" onClick={() => setCount(0)} >Reset to 0</button>
+      </div>
+    </>
   );
 }
 
